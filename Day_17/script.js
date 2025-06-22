b7811c1fafc3d10743e12a6f7104d20d89342b00
SAN_DESH63 <sandeshsharma924@gmail.com>
2025-06-22 14:23:03 +0545

    add Day 17 files

---
+++ Day_17/script.js
@@ -0,0 +1,31 @@
+console.clear()
+console.clear()
+
+let number1 = 10
+let number2 = 5
+
+
+console.log(5 == "5")
+
+if (number1 > number2) {
+    console.log(number1, "is greater than", number2)
+} else {
+    console.log(number2, "is greater than", number1)
+}
+
+hello()
+greet("sandesh")
+
+function hello() {
+    console.log("Hello, how are you?");
+}
+
+function greet(_name) {
+    console.log("Welcome", _name)
+}
+
+function sum(a,b) {
+    console.log(a+b);
+}
+
+// EOF
