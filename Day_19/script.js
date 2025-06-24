7d00c46abf1ac9bcac29ee962e57ac9be856155a
SAN_DESH63 <sandeshsharma924@gmail.com>
2025-06-24 14:26:51 +0545

    add day 19

---
+++ Day_19/script.js
@@ -0,0 +1,48 @@
+console.clear()
+
+// const evenOrOdd = (a) => {
+//     if (a == undefined) {
+//         console.log("give a number");
+//     }
+//     else {
+//         if (a % 2 === 0) {
+//             console.log("even");
+//         } else {
+//             console.log("odd");
+//         }
+//     }
+// }
+// evenOrOdd(32)
+
+// arrray
+
+// const marks = [11,22,33,66,];
+
+// marks.map((_val, index) => {
+//     console.log(marks[index]);
+// })
+
+
+// let num = 10 // global num 
+
+// console.log(num); // 
+
+// function cal(num) {
+//     num = 20
+// }
+
+// cal(num)
+// console.log(num);
+
+// const names = ["Sandesh", "Sameer", "Sumit"]
+
+// names.pop()
+// names.shift()
+
+// names.push("H")
+// names.unshift("Hello")
+
+// names.forEach(_name => {
+//     console.log(_name);
+// })
+
