f6e386989cfd460a3412aad1b6ab25fce6b97c54
SAN_DESH63 <sandeshsharma924@gmail.com>
2025-07-02 13:24:41 +0545

    Add Day 24

---
+++ Day_24/script.js
@@ -0,0 +1,15 @@
+// let htag = document.getElementById("htag");
+// let tags = document.getElementsByClassName("tag") 
+// let spanTag = document.getElementById("spanTag")
+
+// htag.innerText = "sandesh"
+
+// spanTag.innerText = "Sandesh Paudel"
+
+let ptag = document.getElementById("ptag")
+
+let spanTag = document.createElement("span")
+
+spanTag.innerText = "Shaka laka boom boom"
+
+ptag.appendChild(spanTag)
